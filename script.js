585ac2aa52a0ebbddd268b348f2612db08b6bd23
ODubrovskyi <dubrovsky666@gmail.com>
2019-06-01 21:39:35 +0300

    first game mech

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
-window.onload = () => {
-    alert("Hi there!")
+const questions = [
+    {
+        quest: "What is this?",
+        ans: [
+            "Nothing","Something", "Not your business", "Who knows"
+        ],
+        correct: 2
+    }];
+
+const form = document.getElementById("questionForm");
+
+function playGame() {
+    let q = questions[0];
+
+    form.innerHTML += `<h3>${q["quest"]}</h3>`;
+    for (let i = 0; i < q["ans"].length; i++) {
+        form.innerHTML += `<input type="radio" name="question" value="${i}">${q["ans"][i]}<br>`;      
+    }
+
+    form.innerHTML += `<input type="submit" value="Submit">`;
 }
+
+playGame();
