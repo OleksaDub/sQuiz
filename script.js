a9fd26fc1018a1e28b53503d820b817318f2979e
ODubrovskyi <dubrovsky666@gmail.com>
2019-06-02 23:14:58 +0300

    Basic gameplay

--- script.js
+++ script.js
@@ -1,3 +1,44 @@
+let score = 0;
+let point = 1;
+let q;
+const scoreboard = document.getElementById("score");
+scoreboard.innerText = score;
+
+
+// return the value of the radio button that is checked
+// return an empty string if none are checked, or
+// there are no radio buttons
+function getCheckedValue(radioObj) {
+	if(!radioObj)
+		return "";
+	var radioLength = radioObj.length;
+	if(radioLength == undefined)
+		if(radioObj.checked)
+			return radioObj.value;
+		else
+			return "";
+	for(var i = 0; i < radioLength; i++) {
+		if(radioObj[i].checked) {
+            var checkedButton = radioObj[i].value;
+
+            refreshScoreBoard(checkedButton);
+
+			return checkedButton;
+		}
+	}
+	return "";
+}
+
+function refreshScoreBoard(checkedButton) {
+    if(checkedButton == q["correct"]) {
+        score += point;
+        scoreboard.innerText = score;
+    } else {
+        scoreboard.innerText = "Looser!";
+    }
+    return;
+}
+
 const questions = [
     {
         quest: "What is this?",
@@ -7,17 +48,19 @@ const questions = [
         correct: 2
     }];
 
-const form = document.getElementById("questionForm");
+const form = document.getElementById("radiobuttons");
 
 function playGame() {
-    let q = questions[0];
+    q = questions[0];
 
     form.innerHTML += `<h3>${q["quest"]}</h3>`;
     for (let i = 0; i < q["ans"].length; i++) {
         form.innerHTML += `<input type="radio" name="question" value="${i}">${q["ans"][i]}<br>`;      
     }
-
-    form.innerHTML += `<input type="submit" value="Submit">`;
+    
+    let answer = getCheckedValue(document.forms['questionForm'].elements['question']);
+    //let answer = document.querySelector("input[name='question']:checked").val();    
+    
 }
 
 playGame();
